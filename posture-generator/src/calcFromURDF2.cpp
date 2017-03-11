b18c2a204b5290ac5eb5a98419dd4ffbc0e52b0e
Shuhei_YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2017-03-11 16:48:09 +0900

    Added fk sample to posture-generator

--- posture-generator/src/calcFromURDF2.cpp
+++ posture-generator/src/calcFromURDF2.cpp
@@ -4,6 +4,15 @@
 #include <RBDyn/MultiBodyGraph.h>
 #include <RBDyn/MultiBodyConfig.h>
 #include <RBDyn/FK.h>
+#include <RBDyn/FV.h>
+#include <RBDyn/ID.h>
+#include <RBDyn/FD.h>
+#include <RBDyn/CoM.h>
+
+#include <sensor_msgs/JointState.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <random>
+
 
 //for compatibility of indigo and kinetic
 #include <memory>
@@ -39,7 +48,23 @@ void treeParse(_LinkConstSharedPtr link, int level = 0)
             std::cout << "root link: " << link->name << "has no child " << *child << std::endl;
         }
     }
+}
 
+void msgFromMultiBodyConfig(rbd::MultiBody mb, rbd::MultiBodyConfig mbc, sensor_msgs::JointState& msg)
+{
+    //int rbd::MultiBody::sBodyIndexByName(name)
+    //int rbd::MultiBody::sJointIndexByName(name)
+    //sensor_msgs::JointState msg;
+    int count = 0;
+    for (auto itr = mbc.q.begin(); itr != mbc.q.end(); ++itr) {
+        if (mb.joint(count).type() == rbd::Joint::Type::Rev ||
+            mb.joint(count).type() == rbd::Joint::Type::Prism) {// 1dof joint
+            msg.name.push_back(mb.joint(count).name());
+            msg.position.push_back(*(itr->begin()));
+        } 
+        count++;
+    }
+    msg.header.stamp = ros::Time::now();
 }
 
 rbd::MultiBodyGraph mbg;
@@ -107,11 +132,17 @@ void setMultiBodyGraph(_LinkConstSharedPtr link)
             Vector3d axis;
             bool typecheck = true;
             switch (joint->type) { //urdf joint define
-                case Joint::REVOLUTE: {
+                case Joint::REVOLUTE: { //limits should be added
                     type = rbd::Joint::Rev;
                     axis << joint->axis.x, joint->axis.y, joint->axis.z;
                 }
                 break;
+                case Joint::CONTINUOUS: { //no-limit rotation
+                    type = rbd::Joint::Rev;
+                    axis << joint->axis.x, joint->axis.y, joint->axis.z;
+                }
+                break;
+
                 case Joint::FIXED: {
                     type = rbd::Joint::Fixed;
                     axis = Vector3d::UnitZ();
@@ -152,49 +183,60 @@ void setMultiBodyGraph(_LinkConstSharedPtr link)
     return;  
 }
 
-void sample1()
-{
-    // Make rbd::MultiBody instance to use some function such as kinematics
-    // MakeMultiBody("base link name", joint type from world to base link
-    rbd::MultiBody mb = mbg.makeMultiBody("base_link", rbd::Joint::Fixed);
 
-    // Make rbd::MultiBodyConfig instance, which include configuration
+
+void sample3(sensor_msgs::JointState &jmsg, visualization_msgs::MarkerArray &amsg)
+{
+    rbd::MultiBody mb = mbg.makeMultiBody("base_link", rbd::Joint::Fixed);
     rbd::MultiBodyConfig mbc(mb);
-    mbc.zero(mb);
-    // Order of variable is same as joint order
-    int count = 0;
+    std::random_device rnd;
+    std::mt19937 mt(rnd());
+    std::uniform_int_distribution<int> ranj(0,100);
     for (auto itr = mbc.q.begin(); itr != mbc.q.end(); ++itr) {
-        cout << mb.joint(count).name();
-        // access config vector q of joint(count)
         for (auto itr2 = itr->begin(); itr2 != itr->end(); ++itr2) {
-            cout << ", " << *itr2;
+            *itr2 = ranj(mt)/100.0; 
+            //*itr2 = 0.0; 
         }
-        cout << ":" << endl;
-        count++;
     }
-}
-
-void sample2()
-{
-    rbd::MultiBody mb = mbg.makeMultiBody("base_link", rbd::Joint::Fixed);
-    rbd::MultiBodyConfig mbc(mb);
-    mbc.zero(mb);
 
     rbd::forwardKinematics(mb, mbc);
     int count = 0;
+    auto stamp = ros::Time::now();
     for (auto itr = mbc.bodyPosW.begin(); itr != mbc.bodyPosW.end(); ++itr) {
-        cout << mb.body(count).name() << endl;
-        cout << "transration" << endl << itr->translation() << endl;
-        cout << "rotation" << endl << itr->rotation() << endl;
+        //cout << mb.body(count).name() << endl;
+        //cout << "transration" << endl << itr->translation() << endl;
+        //cout << "rotation" << endl << itr->rotation() << endl;
+
+        visualization_msgs::Marker mrk;
+        mrk.header.stamp = stamp;
+        mrk.header.frame_id = "base_link";
+        mrk.id = count;
+        mrk.text = mb.body(count).name();
+        mrk.type = visualization_msgs::Marker::ARROW;
+        mrk.pose.position.x = itr->translation()(0);
+        mrk.pose.position.y = itr->translation()(1);
+        mrk.pose.position.z = itr->translation()(2);
+        auto q = Eigen::Quaterniond(itr->rotation());
+        mrk.pose.orientation.x = q.x();
+        mrk.pose.orientation.y = q.y();
+        mrk.pose.orientation.z = q.z();
+        mrk.pose.orientation.w = -q.w();//Consistency for TF
+        mrk.scale.x = 0.1; mrk.scale.y = 0.01; mrk.scale.z = 0.01;
+        mrk.color.r = 0.5; mrk.color.g = 0.0; mrk.color.b = 0.0; mrk.color.a = 0.5; 
+        amsg.markers.push_back(mrk);
+
         count++;
     }
 
+    msgFromMultiBodyConfig(mb, mbc, jmsg);
 }
 
 int main (int argc, char** argv)
 {
-    ros::init(argc, argv, "multiLinkCalc");
+    ros::init(argc, argv, "multiLinkCalc2");
     ros::NodeHandle n;
+    ros::Publisher j_pub = n.advertise<sensor_msgs::JointState>("joint_states", 1);
+    ros::Publisher a_pub = n.advertise<visualization_msgs::MarkerArray>("markers", 1);
     
     readUrdf();
     _LinkConstSharedPtr root_link = model.getRoot();
@@ -202,11 +244,19 @@ int main (int argc, char** argv)
     setGraph(root_link);
     cout << "graph is already set" << endl;
 
-    cout << "sample1" << endl;
-    sample1();
-    cout << "sample2" << endl;
-    sample2();
+    cout << "sample3" << endl;
+    while (ros::ok()) {
+        sensor_msgs::JointState jmsg;
+        visualization_msgs::MarkerArray amsg;
+        sample3(jmsg, amsg);
+        a_pub.publish(amsg);
+        j_pub.publish(jmsg);
+        ros::Duration(1.0).sleep();
+    }
+    cout << "end " << endl;
+
     ros::spin();
 
     return 0;
 }
+
