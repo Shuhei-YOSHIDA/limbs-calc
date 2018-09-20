2666926c60733fda833a6f1b3d76b9708e8915df
Shuhei_YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2018-09-20 22:50:00 +0900

    sample1 for motion_optimization

--- motion_optimization/include/motion_optimization/preparation.h
+++ motion_optimization/include/motion_optimization/preparation.h
@@ -3,15 +3,23 @@
  */
 #pragma once
 #include <ros/ros.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <eigen_conversions/eigen_msg.h>
+#include <sensor_msgs/JointState.h>
+#include <visualization_msgs/MarkerArray.h>
 #include <RBDyn/MultiBody.h>
 #include <RBDyn/MultiBodyConfig.h>
 #include <mc_rbdyn_urdf/urdf.h>
 
 using namespace std;
 using namespace rbd;
+using namespace sva;
 using namespace mc_rbdyn_urdf;
+using namespace sensor_msgs;
+using namespace visualization_msgs;
 
-void init(MultiBody& mb)
+bool init(MultiBody& mb)
 {
   // Read ros parameter and prepair RobotConfiguration class
   string urdf_string;
@@ -19,8 +27,7 @@ void init(MultiBody& mb)
   if (!nh.getParam("robot_description", urdf_string))
   {
     ROS_ERROR("Urdf is not read: RobotConfiguration::RobotConfiguration()");
-    ros::shutdown();
-    return;
+    return false;
   }
   bool fixed = false;
   vector<string> filteredLinksIn = {};
@@ -28,6 +35,65 @@ void init(MultiBody& mb)
   string baseLinkIn = "base_link";
   auto urdf_parser_res = rbdyn_from_urdf(urdf_string, fixed, filteredLinksIn, transformInertia, baseLinkIn);
   mb = urdf_parser_res.mb;
+
+  return true;
 }
 
+void jointStateFromMBC(MultiBody mb, MultiBodyConfig mbc,
+                       JointState& msg)
+{
+  msg.name.clear();
+  msg.position.clear();
+  msg.velocity.clear();
+  msg.effort.clear();
 
+  int count = 0;
+  for (auto itr = mbc.q.begin(); itr != mbc.q.end(); ++itr)
+  {
+    if (mb.joint(count).type() == rbd::Joint::Type::Rev ||
+      mb.joint(count).type() == rbd::Joint::Type::Prism) // 1dof joint
+    {
+      msg.name.push_back(mb.joint(count).name());
+      msg.position.push_back(*(itr->begin()));
+    }
+    count++;
+  }
+  msg.header.stamp = ros::Time::now();
+}
+
+void markerSet(PTransformd marker_pose, Marker& msg,
+               int marker_id, string frame_id)
+{
+  msg.header.stamp = ros::Time::now();
+  msg.header.frame_id = frame_id;
+  msg.id = marker_id;
+  msg.type = Marker::ARROW;
+  tf::pointEigenToMsg(marker_pose.translation(), msg.pose.position);
+  auto q = Eigen::Quaterniond(marker_pose.rotation());
+  q.w() = -q.w();//Consistency for TF
+  tf::quaternionEigenToMsg(q, msg.pose.orientation);
+  msg.scale.x = 0.2; msg.scale.y = 0.02; msg.scale.z = 0.02;
+  msg.color.r = 0.5; msg.color.g = 0.0; msg.color.b = 0.0; msg.color.a = 1.0;
+}
+
+void broadcastRoot(PTransformd root_pose, string root_id="odom", string base_link_id="base_link")
+{
+  static tf2_ros::TransformBroadcaster br;
+  geometry_msgs::TransformStamped transformStamped;
+  
+  transformStamped.header.stamp = ros::Time::now();
+  transformStamped.header.frame_id = root_id;
+  transformStamped.child_frame_id = base_link_id;
+  auto tra = root_pose.translation();
+  transformStamped.transform.translation.x = tra[0];
+  transformStamped.transform.translation.y = tra[1];
+  transformStamped.transform.translation.z = tra[2];
+  auto q = Eigen::Quaterniond(root_pose.rotation());
+  q.w() = -q.w();
+  transformStamped.transform.rotation.x = q.x();
+  transformStamped.transform.rotation.y = q.y();
+  transformStamped.transform.rotation.z = q.z();
+  transformStamped.transform.rotation.w = q.w();
+
+  br.sendTransform(transformStamped);
+}
--- motion_optimization/src/sample1.cpp
+++ motion_optimization/src/sample1.cpp
@@ -2,27 +2,116 @@
  * @file sample1.cpp
  */
 #include <ros/ros.h>
+#include <sensor_msgs/JointState.h>
+#include <visualization_msgs/MarkerArray.h>
+
 #include "motion_optimization/preparation.h"
 #include <SpaceVecAlg/SpaceVecAlg>
 #include <RBDyn/EulerIntegration.h>
 #include <RBDyn/FK.h>
-#include <RBDyn/FE.h>
+#include <RBDyn/FV.h>
 #include <RBDyn/ID.h>
 
 #include <sch/S_Object/S_Sphere.h>
 #include <sch/S_Object/S_Box.h>
 #include <sch/CD/CD_Pair.h>
 
-#include <Tasks/Bound.h>
+#include <Tasks/Bounds.h>
 #include <Tasks/QPConstr.h>
 #include <Tasks/QPContactConstr.h>
 #include <Tasks/QPSolver.h>
 #include <Tasks/QPTasks.h>
 
+using namespace std;
+using namespace rbd;
+using namespace sva;
+using namespace Eigen;
+using namespace tasks;
+using namespace sensor_msgs;
+using namespace visualization_msgs;
+
 int main(int argc, char** argv)
 {
+  ros::init(argc, argv, "sample1");
+  ros::NodeHandle nh;
+  ros::Publisher js_pub = nh.advertise<JointState>("joint_states", 1);
+  ros::Publisher ma_pub = nh.advertise<MarkerArray>("markers", 1);
+
   MultiBody mb;
-  init(mb);
+  if(!init(mb)) return -1;
+  MultiBodyConfig mbc(mb);
+  mbc.zero(mb);
+
+  forwardKinematics(mb, mbc);
+  forwardVelocity(mb, mbc);
   
+  vector<MultiBody> mbs = {mb};
+  vector<MultiBodyConfig> mbcs(1);
+
+  qp::QPSolver solver;
+  solver.nrVars(mbs, {/*接触情報?*/}, {});
+
+  solver.updateConstrSize();
+
+  auto pos0 = Vector3d::Zero();
+  qp::PositionTask posTask0(mbs, 0, "base_link", pos0);
+  qp::SetPointTask posTaskSp0(mbs, 0, &posTask0, 10., 1);
+  solver.addTask(&posTaskSp0);
+
+  auto pos1 = Vector3d(0.300, 0.300, 0.300);
+  qp::PositionTask posTask1(mbs, 0, "l_wrist", pos1);
+  qp::SetPointTask posTaskSp1(mbs, 0, &posTask1, 10., 1);
+  solver.addTask(&posTaskSp1);
+
+  mbcs[0] = mbc;
+  vector<MultiBodyConfig> mbc_array(10000);
+  vector<JointState> js_array(10000);
+  vector<PTransformd> root_pose_array(10000);
+  for (int i = 0; i < 10000; i++)
+  {
+    if(!solver.solve(mbs, mbcs))
+    {
+      ROS_FATAL("Could not solved");
+      ros::shutdown();
+      return -1;
+    }
+    eulerIntegration(mb, mbcs[0], 0.001);
+
+    forwardKinematics(mb, mbcs[0]);
+    forwardVelocity(mb, mbcs[0]);
+
+    mbc_array[i] = mbcs[0];
+    JointState js_msg;
+    jointStateFromMBC(mb, mbcs[0], js_msg);
+    js_array[i] = js_msg;
+    root_pose_array[i] = mbcs[0].bodyPosW[mb.bodyIndexByName("base_link")];
+  }
+
+  ROS_INFO_STREAM("Task error: " << posTask1.eval().norm());
+
+  Marker m_msg;
+  markerSet(PTransformd(pos1), m_msg, 0, "odom");
+  MarkerArray ma_msg;
+  ma_msg.markers.push_back(m_msg);
+
+  ros::Rate loop(1000);
+  while(ros::ok())
+  {
+    ROS_INFO("replay motion");
+    for (auto&& m : ma_msg.markers) m.header.stamp = ros::Time::now();
+    ma_pub.publish(ma_msg);
+    for (int i = 0; i < 10000; i++)
+    {
+      auto stamp = ros::Time::now();
+      js_array[i].header.stamp = stamp;
+      js_pub.publish(js_array[i]);
+      broadcastRoot(root_pose_array[i]);
+      loop.sleep();
+    }
+    ROS_INFO("finished replay");
+  }
+
+  ros::spin();
+
   return 0;
 }
